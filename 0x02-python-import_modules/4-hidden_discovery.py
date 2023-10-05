a4c7ef74fb0ebc67319e8aec7d0e88b8d804c573
johnahumanbeing <mseewak@gmail.com>
2023-10-05 23:43:03 +0300

    fixing later

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import hidden_4
+    modyule = dir(hidden_4)
+    for name in modyule:
+        if name[0:2] != "__":
+            print("{:s}".format(modyule))
