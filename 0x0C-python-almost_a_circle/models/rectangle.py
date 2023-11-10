eaacabc496a0f5323d809382f9db8f8bec806e01
johnahumanbeing <mseewak@gmail.com>
2023-11-11 01:24:44 +0300

    chef of the strings

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,3 +106,10 @@ class Rectangle(Base):
         """
         for j in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """
+        Returns a string rep of the rectangle
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
