6ddebb7cbf109453bc17a8c8609b5cb250eff364
johnahumanbeing <mseewak@gmail.com>
2023-11-14 00:20:33 +0300

    validating attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,12 +37,11 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for width atrribute"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -52,12 +51,11 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height atribute"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
             raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def x(self):
