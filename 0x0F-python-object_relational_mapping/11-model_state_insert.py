009eac173b5c6db8fb57da855788002ab0393c82
johnahumanbeing <mseewak@gmail.com>
2024-01-13 23:38:00 +0300

    script that adds the State object “Louisiana” to the database hbtn_0e_6_usa (fix 1)

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -14,13 +14,11 @@ if __name__ == "__main__":
     USR = argv[1]
     PASS = argv[2]
     DB = argv[3]
-
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(USR, PASS, DB))
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
     state = State(name="Lousiana")
 
     session.add(state)
