d141c1a89edfd4395e1a6029ac67d4258c984fcf
johnahumanbeing <mseewak@gmail.com>
2024-01-14 22:51:47 +0300

    script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa:

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,9 +3,9 @@
 contains the definition of a city
 """
 
-from sqlalchemy.orm import Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
